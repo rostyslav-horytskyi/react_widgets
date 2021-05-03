d56eb3eefa8aad042a561c63114aa4cb9a3fef13
gorzer <gorzer.rostyslav@gmail.com>
2021-05-03 16:28:11 +0300

    Add event handler onTitleClick

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 export const Accordion = ({ items }) => {
+  const onTitleClick = index => {
+    console.log('Title clicked', index);
+  };
+
   return (
-    <div>Accordion</div>
+    <div className="ui styled accordion">
+      {items.map((item, index) => (
+        <React.Fragment key={item.title}>
+          <div
+            className="title active"
+            onClick={() => onTitleClick(index)}
+          >
+            <i className="dropdown icon"></i>
+            {item.title}
+          </div>
+          <div className="content active">
+            <p>{item.content}</p>
+          </div>
+        </React.Fragment>
+      ))}
+    </div>
   );
 };
