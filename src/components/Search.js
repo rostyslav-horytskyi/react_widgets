118f8ad12e750d0cf52171d3638504182c28a342
gorzer <gorzer.rostyslav@gmail.com>
2021-05-04 14:41:27 +0300

    Problem with CORS

--- src/components/Search.js
+++ src/components/Search.js
@@ -13,6 +13,9 @@ export const Search = () => {
         origin: '*',
         format: 'json',
         srsearch: term,
+        headers: {
+          "Access-Control-Allow-Origin": '*',
+        }
       });
 
       setResults(data.query.search);
@@ -25,11 +28,21 @@ export const Search = () => {
 
   const renderedResults = results.map(result => (
     <div key={result.pageid} className="item">
+      <div className="right floated content">
+        <a
+          className="ui button"
+          href={`https://en.wikipedia.org?curid=${result.pageid}`}
+        >
+          Go
+        </a>
+      </div>
       <div className="content">
         <div className="header">
           {result.title}
         </div>
-        {result.snippet}
+        <span dangerouslySetInnerHTML={{
+          __html: result.snippet,
+        }}></span>
       </div>
     </div>
   ));
