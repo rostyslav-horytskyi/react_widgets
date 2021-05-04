07c50846ae3a8eadf3d04fabb539457a4e5bf625
gorzer <gorzer.rostyslav@gmail.com>
2021-05-04 10:41:56 +0300

    Get data from server

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export const Search = () => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        action: 'query',
+        list: 'search',
+        origin: '*',
+        format: 'json',
+        srsearch: term,
+      });
+
+      setResults(data.query.search);
+    };
+
+    if (term) {
+      search();
+    }
+  }, [term]);
 
   return (
     <div>
